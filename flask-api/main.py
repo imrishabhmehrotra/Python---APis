5043b79dafaa8b1c7935cfcd760119d411ea130f
Rishabh Mehrotra <rishabh.2024en1164@kiet.edu>
2024-02-10 19:02:38 +0530

    A python API created using Flask

---
+++ flask-api/main.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+@app.route('/api/greet', methods=['GET'])
+def greet():
+  return jsonify(message="Hello, Namaste!")
+
+if __name__ == '__main__':
+  app.run(debug = True)
